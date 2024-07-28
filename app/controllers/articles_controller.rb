c27c398a76eb826c8c1c67d91022c2145ea27663
Cyril Mosawan <cjmosawan@yahoo.ie>
2024-07-28 19:05:21 +0100

    replaced get routes in routes.rb with resources

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,11 @@ class ArticlesController < ApplicationController
     @articles = Article.all
     #Fetch all articles from DB
   end
+
+  def show
+    @article = Article.find(params[:id])
+    #Calls article.find which uses the id in the path
+    # The value is then stored in the @article variable
+    # Where it can be accessed
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
-  #Line says that GET /articles requests are mapped
+  #Maps the root path of our app to the appropriate
+  # controller action
+
+
+  # get "/articles", to: "articles#index"
+  # Line says that GET /articles requests are mapped
   # to the index action of ArticlesController
+
+  # get "/articles/:id", to: "articles#show"
+  # Get route that includes :id which is a route parameter
+  # Can also be used as a parameter value in the
+  # show action of ArticlesController
+
+  resources :articles
 end
