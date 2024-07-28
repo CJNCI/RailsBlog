e7d68c2cad1ca823458a2c00920611fc98c3f5e3
Cyril Mosawan <cjmosawan@yahoo.ie>
2024-07-28 18:40:47 +0100

    landing page prints article values

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+    #Fetch all articles from DB
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "articles#index"
   get "/articles", to: "articles#index"
   #Line says that GET /articles requests are mapped
   # to the index action of ArticlesController
