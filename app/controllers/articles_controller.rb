8880a5e58489e82d373903148e698f1c592a9047
Cyril Mosawan <cjmosawan@yahoo.ie>
2024-07-28 20:29:42 +0100

    added code to create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,21 @@ class ArticlesController < ApplicationController
     # The value is then stored in the @article variable
     # Where it can be accessed
   end
+
+  def new #Instantiates new article but doesnt save
+    @article = Article.new
+  end
+
+  def create #Instantiates new article with values for title and body and tries to save
+
+    @article = Article.new(title: "...", body: "...") # Dummy values
+
+    #If article is saved successfully, browser redirected to article's page
+    # Else, action redisplays the form with status code Unprocessable Entity
+    if @article.save
+      redirect_to article
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
 end
