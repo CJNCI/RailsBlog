0b79b3a890d7bfd3f079cf5de6705003def77f90
Cyril Mosawan <cjmosawan@yahoo.ie>
2024-07-30 23:13:10 +0100

    fixing lint issues

--- out/test/blog/application_system_test_case.rb
+++ out/test/blog/application_system_test_case.rb
@@ -1,9 +1,7 @@
-# This shiny device polishes bared foos
-
 # frozen_string_literal: true
 
 require 'test_helper'
-
+# This shiny device polishes bared foos
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
